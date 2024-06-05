e2c0c7d7206ac033e338d9a728181ec271055250
Deekshith <deekshithpatel10@gmail.com>
2024-06-05 11:51:53 +0530

    Solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+   const resultArray = [];
+
+   // since the number of elements to be removed is not known,
+   // we use the 'argument' object of the function.
+
+   const ElementsToRemove = Array.from(arguments).slice(1);
+   
+
+   for( index = 0; index < array.length; index++) {
+      if ( ElementsToRemove.includes( array[index] ) ) continue;
+      else {
+         resultArray.push(array[index]);
+      }
+   }
+
+   return resultArray;
 };
 
 // Do not edit below this line
