7f4fa87f474f9b2b41c313271a79434e13bfc499
Deekshith <deekshithpatel10@gmail.com>
2024-06-05 10:59:40 +0530

    Solve repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(text, numOfRepeats) {
+   let finalText = "";
 
+   if ( numOfRepeats < 0 ) return "ERROR";
+
+   for( let index = 0; index < numOfRepeats; index++ ) {
+      finalText += text;
+   }
+
+   return finalText;
 };
 
 // Do not edit below this line
