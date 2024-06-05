d2cec08a62ed417d2d4a2167f8c8a55042674973
Deekshith <deekshithpatel10@gmail.com>
2024-06-05 11:18:24 +0530

    Solve 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(text) {
+   let reverseText = "";
+   
+   for( index = 0; index < text.length; index++) {
+      reverseText += text[ text.length - 1 - index ];
+   }
 
+   return reverseText;
 };
 
 // Do not edit below this line
