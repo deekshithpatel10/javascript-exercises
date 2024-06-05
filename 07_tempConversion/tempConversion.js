4b53aa213d2632e6266c15346c1af78536d90eab
Deekshith <deekshithpatel10@gmail.com>
2024-06-05 13:04:38 +0530

    Solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahren) {
+  let celsius = ( fahren - 32 ) * (5/9);
+
+  return +celsius.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahren = ( ( celsius * 9 ) / 5 ) + 32;
+
+  return +fahren.toFixed(1);
 };
 
 // Do not edit below this line
